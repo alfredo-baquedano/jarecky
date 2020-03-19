8078277b3da32ae2015cd45752cff1da143087c7
Alfredo <alfredo.baquedano.c@gmail.com>
2020-03-19 15:55:16 -0300

    init commit

---
+++ App.js
@@ -0,0 +1,53 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the UI Kitten template
+ * https://github.com/akveo/react-native-ui-kitten
+ *
+ * Documentation: https://akveo.github.io/react-native-ui-kitten/docs
+ *
+ * @format
+ */
+
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import {
+  ApplicationProvider,
+  Button,
+  Icon,
+  IconRegistry,
+  Layout,
+  Text,
+} from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import {
+  mapping,
+  dark as theme,
+} from '@eva-design/eva';
+
+const App = () => (
+  <>
+    <IconRegistry icons={EvaIconsPack}/>
+    <ApplicationProvider mapping={mapping} theme={theme}>
+      <Layout style={styles.container}>
+        <Text style={styles.text} category='h1'>
+          Welcome to Jarecki
+        </Text>
+      </Layout>
+    </ApplicationProvider>
+  </>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    textAlign: 'center',
+  }
+});
+
+export default App;
