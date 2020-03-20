56dfefaa70ccc75149572d62d68a90c4359db8ee
Alfredo <alfredo.baquedano.c@gmail.com>
2020-03-20 00:03:33 -0300

    added charts

--- components/ChartScreen.js
+++ components/ChartScreen.js
@@ -1,19 +1,85 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-import { StyleSheet } from 'react-native';
+import { StyleSheet, processColor } from 'react-native';
+
+/*
+import {
+    BarChart
+} from "react-native-chart-kit";
+*/
+
+import { CandleStickChart } from 'react-native-charts-wrapper';
 
 import {
     Layout,
     Button,
 } from '@ui-kitten/components';
 
-export default class ChartScreen extends Component {
+import {
+    dark as theme,
+} from '@eva-design/eva';
 
-    render() {
-        return (
-        <Layout style={styles.container}>
-            <Layout style={styles.chartLayour}>
+const ChartScreen = () => {
 
+    const [chartWidth, setChartWidth] = useState(400);
+    const [chartHeight, setChartHeight] = useState(200);
+
+    const legend = {
+        enabled: false,
+        textSize: 14,
+        form: 'CIRCLE',
+        wordWrapEnabled: true
+    }
+    const [chartData, setChartData] = useState({
+        dataSets: [{
+            values: [
+                {shadowH: 1, shadowL: 0, open: 1, close: 0},
+                {shadowH: 2, shadowL: 1, open: 2, close: 1},
+                {shadowH: 3, shadowL: 2, open: 3, close: 2},
+                {shadowH: 2, shadowL: 3, open: 2, close: 3},
+                {shadowH: 1, shadowL: 2, open: 1, close: 2},
+                {shadowH: 2, shadowL: 1, open: 2, close: 1},
+            ],
+            label: 'AAPL',
+            config: {
+                highlightColor: processColor('darkgray'),
+
+                shadowColor: processColor('black'),
+                shadowWidth: 1,
+                shadowColorSameAsCandle: true,
+                increasingColor: processColor('#71BD6A'),
+                increasingPaintStyle: 'FILL',
+                decreasingColor: processColor('#D14B5A')
+            },
+            xAxis: {},
+            yAxis: {}
+        }],
+    })
+    const marker = {
+        enabled: true,
+        markerColor: processColor('#2c3e50'),
+        textColor: processColor('white'),
+    }
+
+    return (
+        <Layout style={styles.container} onLayout={(event) => {
+            setChartHeight(event.nativeEvent.layout.height)
+        }}>
+            <Layout style={styles.chartLayour} onLayout={(event) => {
+                setChartWidth(event.nativeEvent.layout.width)
+            }}>
+            <CandleStickChart
+                style={{height: chartHeight, width: chartWidth}}
+                data={chartData}
+                marker={marker}
+                chartDescription={{text: 'CandleStick'}}
+                legend={legend}
+                xAxis={{}}
+                yAxis={{}}
+                maxVisibleValueCount={16}
+                autoScaleMinMaxEnabled={true}
+                zoom={{scaleX: 1, scaleY: 1, xValue:  40, yValue: 916, axisDependency: 'LEFT'}}
+            />
             </Layout>
             <Layout style={styles.chartControlLayour}>
                 <Button style={styles.chartControlButtonUp}>
@@ -24,8 +90,7 @@ export default class ChartScreen extends Component {
                 </Button>
             </Layout>
         </Layout>
-        )
-    }
+    )
 }
 
 const styles = StyleSheet.create({
@@ -36,12 +101,12 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     chartControlLayour: {
-        flex: 1,
+        flexShrink: 1,
         justifyContent: 'center',
         alignItems: 'center',
     },
     chartLayour: {
-        flex: 1,
+        flexGrow: 1,
         justifyContent: 'center',
         alignItems: 'center',
     },
@@ -59,5 +124,10 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         borderColor: 'white',
         margin: 20
+    },
+    chart: {
+        flex: 1
     }
 });
+
+export default ChartScreen
